db82ecac912c7bb8bc70f55c2f19b2b9e874a0a2
ersadishla <ersadishlahuddin@gmail.com>
2018-12-04 12:40:32 +0700

    Initial commit

---
+++ application/views/about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+<body>
+	<div class="container">
+		<div class="jumbotron text-center bg-dark text-white">
+			<h1 class="border border-light p-3 mr-5 ml-5">Toko Buah</h1>
+			<br>
+			<h4>Beli Buah tanpa ribet</h4>
+			<p>Terjamin kesegaran dan rasanya</p>				
+		</div>
+
+	</div>
+	
+</body>
+</html>
+
